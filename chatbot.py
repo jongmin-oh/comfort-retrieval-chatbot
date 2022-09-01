324bdd5f88a340a1d82c5bcb175b26819efeb148
Acdong <alswhddh@naver.com>
2022-09-01 15:23:25 +0900

    comfort_bot

--- chatbot.py
+++ chatbot.py
@@ -1,57 +1,31 @@
 import pandas as pd
 import numpy as np
-import json
 import faiss
-from datetime import datetime
 import random
-from sentence_transformers import SentenceTransformer , util
+from sentence_transformers import SentenceTransformer
 
-import mysql.connector.pooling
-
-#MySQL DB 연결
-with open('./config/db_config.json','r') as f:
-    dbconfig = json.load(f)
-    
-cnxpool = mysql.connector.pooling.MySQLConnectionPool(pool_name = 'mypool',
-                                                      pool_size = 3,
-                                                      pool_reset_session=True,
-                                                      **dbconfig) 
-
-qestion_embedder = SentenceTransformer("Huffon/sentence-klue-roberta-base")
+df = pd.read_excel("./data/base_datasets.xlsx")
+embedder = SentenceTransformer("./models/Huffon_sentence-klue-roberta-base")
 index = faiss.read_index("./faiss/sts.index")
 
-def chatbot_answer(query : str ) -> str :
-    
-    #----------------------------#
-    # Retrieval part             #
-    #----------------------------#
-    qestion_embedding = qestion_embedder.encode(query, normalize_embeddings=True,convert_to_tensor=True)
-    distances, indices = index.search(np.expand_dims(qestion_embedding,axis=0),3)
-    
-    #max distance 가 0.5가 안넘으면 무슨말인지 모르겠어요 하고 NoSQL에 축적
-    if np.min(distances[0]) > 0.6:
-        #----NoSQL로 전송-------#
-        #print(np.max(distances[0]))
-        return "무슨 말인지 잘 모르겠어요 다시 한 번 말씀해주시겠어요??"
-    
-    index_q = [i[1] for i in zip(distances[0],indices[0]) if i[0] <= 0.6]
-    index_q = list(map(lambda x : x+1, index_q))
-    index_q = str(index_q).replace('[','(').replace(']',')')
-    
-    # MySQL DB에서 임베딩 벡터를 검색 Bi-Encoder
-    cnx = cnxpool.get_connection()
-    cursor = cnx.cursor(dictionary=True)
-    
-    sql = f"""SELECT `index` , user, system FROM chatbot WHERE `index` IN {index_q};"""
-    cursor.execute(sql)
-    res = cursor.fetchall()
+THRESHOLD = 50
+SEARCH_AMOUNT = 5
 
-    cnx.close()
-    cursor.close()
-    
-    temp = pd.DataFrame(res)
-    
-    if len(temp) < 2 :
-        return temp.loc[0].system
-    
-    return temp.loc[random.randint(0,1)].system
+
+def chatbot_answer(query):
+    # Retrieval part
+    qestion_embedding = embedder.encode(query, convert_to_tensor=True)
+    input_embedding = np.expand_dims(qestion_embedding, axis=0)
+    distances, indices = index.search(input_embedding, SEARCH_AMOUNT)
+    # max distance 가 임계치를 안넘으면 무슨말인지 모르겠어요 하고 NoSQL에 축적
+    if np.min(distances[0]) > THRESHOLD:
+        # print(np.max(distances[0]))
+        return "무슨 말인지 잘 모르겠어요 다시 한 번 말씀해주시겠어요??"
+    index_q = [i[1]for i in zip(distances[0], indices[0]) if i[0] <= THRESHOLD]
+
+    if len(index_q) < 2:
+        return df.loc[index_q[0]].system
+
+    return df.loc[index_q[random.randint(0, 1)]].system
+
+# print(chatbot_answer("사랑했던 사람이 떠났어"))
--- service.py
+++ service.py
@@ -1,25 +1,40 @@
-from flask import Flask , request , jsonify
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 from chatbot import chatbot_answer
 from kakao_api import KakaoTemplate
+import logging
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 skillTemplate = KakaoTemplate()
 
-app = Flask(__name__)
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.route("/chatbot", methods=['POST'])
-def combotAPI():
+
+class ComportbotRequest(BaseModel):
+    userRequest: str
+
+
+@app.post("/chatbot")
+async def chatbotAPI(request: ComportbotRequest):
     try:
-        receive_data = request.get_json()
-        query = str(receive_data['userRequest']['utterance'])
-        
+        query = str(request.userRequest['utterance'])
         app.logger.info(f"input query : {str(query)}")
         result = chatbot_answer(query)
-        
-        return jsonify(skillTemplate.send_response(result))
+        return skillTemplate.send_response(result)
     except Exception as e:
         result = f"에러 : {e}"
         app.logger.info(f"error : {e}")
-        return jsonify(skillTemplate.send_response(result))
+        return skillTemplate.send_response(result)
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port = 5000,threaded=True,debug=True)
+    app.run(host='0.0.0.0', port=5000, threaded=True, debug=True)
