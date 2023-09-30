97eca065f9e385235bd680d2e787620356a4b195
Acdong <acdong@Acdongui-MacBookAir.local>
2023-09-30 14:25:56 +0900

    [FEAT] push images ecr

---
+++ image_ecr_push.sh
@@ -0,0 +1,10 @@
+if [[ -f .env ]]; then
+    export $(cat .env | xargs)
+fi
+
+docker_base_dir="/opt/${SERVER_NAME}"
+aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin ${AWS_ECR_URL}
+
+docker build --build-arg DIR=$docker_base_dir -t ${SERVER_NAME} .
+docker tag ${SERVER_NAME}:latest ${AWS_ECR_URL}/${SERVER_NAME}:latest
+docker push ${AWS_ECR_URL}/comfort:latest
