6831b88ffe2f6a89bc13fcf911c07811de76cac4
j5ng <alswhddh@naver.com>
2024-01-21 16:36:25 +0900

    [FEAT]

--- refresh.sh
+++ refresh.sh
@@ -8,12 +8,14 @@ fi
 echo "기존 ${SERVER_NAME} 서버를 중지합니다."
 
 # container_name="combot"
-docker_base_dir="/opt/${SERVER_NAME}"
 docker stop ${SERVER_NAME} ; docker rm ${SERVER_NAME} ; docker rmi ${SERVER_NAME}
 
 echo "새로운 ${SERVER_NAME} 서버를 빌드합니다."
-docker build --build-arg DIR=$docker_base_dir -t ${SERVER_NAME} .
+docker build -t ${SERVER_NAME} .
+
+echo "이전 ${SERVER_NAME} 이미지를 삭제합니다."
+docker system prune -af
 
 echo "새로운 ${SERVER_NAME} 서버를 시작합니다."
-docker run --name ${SERVER_NAME} --restart=always --network=bridge -v $PWD:$docker_base_dir -d -p ${PORT}:${PORT} ${SERVER_NAME}:latest
+docker run --name ${SERVER_NAME} --restart=always --network=bridge -v $PWD:/opt/app -d -p ${PORT}:${PORT} ${SERVER_NAME}:latest
 docker logs ${SERVER_NAME} --tail 20 -f
