069f01fdefb760dfd511e1780d20d0be3863f207
j5ng <alswhddh@naver.com>
2024-01-21 16:39:33 +0900

    [FEAT]

--- refresh.sh
+++ refresh.sh
@@ -13,9 +13,10 @@ docker stop ${SERVER_NAME} ; docker rm ${SERVER_NAME} ; docker rmi ${SERVER_NAME
 echo "새로운 ${SERVER_NAME} 서버를 빌드합니다."
 docker build -t ${SERVER_NAME} .
 
+echo "새로운 ${SERVER_NAME} 서버를 시작합니다."
+docker run --name ${SERVER_NAME} --restart=always --network=bridge -v $PWD:/opt/app -d -p ${PORT}:${PORT} ${SERVER_NAME}:latest
+
 echo "이전 ${SERVER_NAME} 이미지를 삭제합니다."
 docker system prune -af
 
-echo "새로운 ${SERVER_NAME} 서버를 시작합니다."
-docker run --name ${SERVER_NAME} --restart=always --network=bridge -v $PWD:/opt/app -d -p ${PORT}:${PORT} ${SERVER_NAME}:latest
 docker logs ${SERVER_NAME} --tail 20 -f
